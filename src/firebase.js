01b9360c7a8972895c8892a304c104e231208cf3
norenaja <jnorena.jr@gmail.com>
2019-04-06 19:08:48 -0400

    deploy firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBEmYmp0FdkDHDKomc7B6JVejurezx57UU',
+  authDomain: 'react-chat-9649a.firebaseapp.com',
+  databaseURL: 'https://react-chat-9649a.firebaseio.com',
+  projectId: 'react-chat-9649a',
+  storageBucket: 'react-chat-9649a.appspot.com',
+  messagingSenderId: '364860090938'
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
